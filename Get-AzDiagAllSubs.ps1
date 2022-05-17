5f8f530bfc307693ae243b3f3fb36c026da0a714
Mark Fink <mark.fink@lacework.net>
2022-05-17 14:40:53 -0400

    activity log diag script

---
+++ Get-AzDiagAllSubs.ps1
@@ -0,0 +1,16 @@
+ForEach ($sub In Get-AzSubscription) {
+    $output = "" | Select-Object Subscription, LogStorage
+    ForEach ($diag In Get-AzDiagnosticSetting -SubscriptionId $sub.SubscriptionId) {
+        If ($diag.StorageAccountId) {
+            $output = "" | Select-Object Subscription, LogStorage
+            $output.Subscription = $sub.Name
+            $output.LogStorage = $diag.StorageAccountId
+            Write-Output $output
+        }
+    }
+    If (-Not $output.Subscription) {
+        $output.Subscription = $sub.Name
+        $output.LogStorage = "None"
+        Write-Output $output
+    }
+}
